66e27e3409514ee42e3d4ebcfb82681f893faa35
Hum Bruno <hsbruno1@gmail.com>
2022-05-21 17:34:06 +0100

    eslint and prettier config, styled components global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import GlobalStyles from './components/styles/Global';
+
 function App() {
   console.log('testing connection');
 
   return (
     <div>
+      <GlobalStyles />
       <h1>Hello</h1>
     </div>
   );
---
+++ src/components/styles/Global.ts
@@ -0,0 +1,11 @@
+import * as styled from 'styled-components';
+
+const GlobalStyles = styled.createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyles;
