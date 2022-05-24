cdbaa3919eed51ab2b38383fdb83fb6741eb5eac
Hum Bruno <hsbruno1@gmail.com>
2022-05-24 21:10:50 +0100

    feat: context

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,16 @@
-import { useEffect, useState } from 'react';
-import FilterTable from './components/FilterTable';
 import JobList from './components/JobList';
 import Container from './components/styles/Container.styled';
 import GlobalStyles from './components/styles/Global';
-
-import Job from './types/Job';
+import { JobContextProvider } from './context/JobContext';
 
 function App() {
-  const [jobs, setJobs] = useState<Job[]>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isFiltered, setIsFiltered] = useState<boolean>(false);
-  const [filteredList, setFilteredList] = useState<string[]>([
-    'JavaScript',
-    'React',
-    'CSS',
-    'TypeScript',
-  ]); // placeholder items in array, to remove
-
-  /*  const addFilterItem = (item: string) => {
-    setFilteredList((currentList) => [...currentList, item]);
-  }; */
-
-  const clickHandler = () => {
-    setIsFiltered(!isFiltered);
-    setFilteredList(filteredList); // to remove later, placeholder only
-  };
-
-  useEffect(() => {
-    fetch('data.json')
-      .then((res) => res.json())
-      .then((data) => {
-        setJobs(data);
-        setIsLoading(false);
-      })
-      .catch((error) => console.log(error.message));
-  }, []);
-
   return (
-    <Container>
+    <JobContextProvider>
       <GlobalStyles />
-      {isFiltered && <FilterTable list={filteredList} />}
-      {isLoading && <p>Loading</p>}
-      <button type="button" onClick={clickHandler}>
-        Filter
-      </button>
-      {jobs && <JobList jobs={jobs} />}
-    </Container>
+      <Container>
+        <JobList />
+      </Container>
+    </JobContextProvider>
   );
 }
 
--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import JobItem from './JobItem';
 
 import List from './styles/JobList.styled';
 
-import Job from '../types/Job';
+import { JobContext } from '../context/JobContext';
+import FilterTable from './FilterTable';
 
-const JobList: React.FC<{ jobs: Job[] }> = ({ jobs }) => (
-  <List>
-    {jobs.map((job) => (
-      <JobItem key={job.id} job={job} />
-    ))}
-  </List>
-);
+const JobList = () => {
+  const { jobs, isLoading } = useContext(JobContext);
+
+  const [isFiltered, setIsFiltered] = useState<boolean>(false);
+  const [filteredList, setFilteredList] = useState<string[]>([
+    'JavaScript',
+    'React',
+    'CSS',
+    'TypeScript',
+  ]); // placeholder items in array, to remove
+
+  const clickHandler = () => {
+    setIsFiltered(!isFiltered);
+    setFilteredList(filteredList); // to remove later, placeholder only
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {isFiltered && <FilterTable list={filteredList} />}
+      <button type="button" onClick={clickHandler}>
+        Filter
+      </button>
+      <List>
+        {jobs.map((job) => (
+          <JobItem key={job.id} job={job} />
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default JobList;
